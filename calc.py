5a6b3fe1ada2da4980c92f7247e5de3e0b61dbf9
jahiadm46-sys <jahiadm46@gmail.com>
2026-03-21 23:50:19 +0600

    Updated calculator

--- calc.py
+++ calc.py
@@ -7,4 +7,10 @@ num2 = input("Enter second number: ")
 # সংখ্যাগুলোকে যোগ করা
 sum = int(num1) + int(num2)
 
+print("The total is: " + str(sum))sub = int(num1) - int(num2)
+mul = int(num1) * int(num2)
+div = int(num1) / int(num2)
 print("The total is: " + str(sum))
+print("The subtraction is: " + str(sub))
+print("The multiplication is: " + str(mul))
+print("The division is: " + str(div))
