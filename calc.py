401a878b9ad4675e287946a7c883138d61fd3663
jahiadm46-sys <jahiadm46@gmail.com>
2026-03-21 23:32:17 +0600

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,10 @@
+# এটি একটি ছোট ক্যালকুলেটর
+print("--- Simple Addition Calculator ---")
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# সংখ্যাগুলোকে যোগ করা
+sum = int(num1) + int(num2)
+
+print("The total is: " + str(sum))
